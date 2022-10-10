e9a7cc79c860f5455e3e474faef8f3a4d4b52091
Suyash Sachdeva <77083945+suyashsachdeva@users.noreply.github.com>
2022-10-10 23:29:05 +0530

    Added Calculator

---
+++ calculator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout<<"Please enter two nuumbers :";
+    cin>>num1>>num2;
+    cout<<"Sum of the 2 numbers is "<<num1+num2;
+    cout<<"Difference of the 2 numbers is "<<num1-num2;
+    cout<<"The product of the 2 numbers is "<<num1*num2;
+    cout<<"The division of the two number is "<<num1/num2;
+    cout<<"The remainder of the division performed above is "<<num1%num2;
+    return 0;
+}
